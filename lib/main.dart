33b64a06843630725d42b3e9e1be6cd1b7ac7b61
hyochan <dooboolab@gmail.com>
2020-12-13 23:32:41 +0900

    Install fonts and organize assets

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter\_localizations/flutter\_localizations.dart';
 
 import './utils/localization.dart';
-import './utils/theme.dart' as Theme;
+import 'utils/asset.dart' as Asset;
 import './views/home.dart' show Home;
 
 void main() => runApp(new MyApp());
@@ -14,31 +14,39 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Theme.Colors.dodgerBlue,
-        accentColor: Theme.Colors.dodgerBlue,
-        hintColor: Theme.Colors.paleGray,
-        disabledColor: Theme.Colors.disabled,
+        brightness: Brightness.dark,
+        backgroundColor: Asset.Colors.darkBackground,
+        fontFamily: 'NotoSans',
+        textTheme: TextTheme(
+          headline1: TextStyle(
+              color: Colors.white, fontSize: 36, fontFamily: 'Timmana'),
+          caption: TextStyle(
+              color: Colors.white, fontSize: 16, fontFamily: 'NotoSans'),
+          bodyText1: TextStyle(
+              color: Colors.white, fontSize: 24, fontFamily: 'NotoSans'),
+        ),
+        primaryColor: Asset.Colors.primary,
+        buttonColor: Asset.Colors.primary,
+        disabledColor: Asset.Colors.disabled,
       ),
       routes: {
         '/home': (BuildContext context) => Home(),
       },
-      supportedLocales: [
-        const Locale('en', 'US'),
-        const Locale('ko', 'KR')
-      ],
+      supportedLocales: [const Locale('en', 'US'), const Locale('ko', 'KR')],
       localizationsDelegates: [
         const LocalizationDelegate(supportedLocales: ['en', 'ko']),
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate
       ],
-      localeResolutionCallback: (Locale locale, Iterable<Locale> supportedLocales) {
+      localeResolutionCallback:
+          (Locale locale, Iterable<Locale> supportedLocales) {
         if (locale == null) {
           debugPrint("*language locale is null!!!");
           return supportedLocales.first;
         }
         for (Locale supportedLocale in supportedLocales) {
-           if (supportedLocale.languageCode == locale.languageCode || supportedLocale.countryCode == locale.countryCode) {
+          if (supportedLocale.languageCode == locale.languageCode ||
+              supportedLocale.countryCode == locale.countryCode) {
             return supportedLocale;
           }
         }
@@ -49,4 +57,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
---
+++ lib/utils/asset.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  Colors._();
+  static const red = Color.fromARGB(255, 255, 114, 141);
+  static const orange = Color.fromARGB(255, 245, 166, 35);
+  static const yellow = Color.fromARGB(255, 238, 208, 0);
+  static const green = Color.fromARGB(255, 29, 211, 168);
+  static const blue = Color.fromARGB(255, 175, 194, 219);
+  static const purple = Color.fromARGB(255, 182, 105, 249);
+  static const disabled = Color.fromARGB(255, 210, 210, 210);
+  static const darkBackground = Color.fromARGB(255, 18, 18, 18);
+  static const primary = Color.fromARGB(255, 68, 82, 254);
+}
+
+class Icons {
+  Icons._();
+  // static var icAppTitle = AssetImage('res/icons/appTitle.png');
+}

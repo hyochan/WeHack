a43b5aa7151e9eb1c75e0687321f84d581c5a615
hyochan <dooboolab@gmail.com>
2020-12-13 13:43:31 +0900

    Configure initial boilerplate

---
+++ lib/views/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../utils/theme.dart' as Theme;
+import '../utils/localization.dart' show Localization;
+import '../utils/general.dart';
+
+class Home extends StatelessWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Empty'),
+      ),
+    );
+  }
+}
